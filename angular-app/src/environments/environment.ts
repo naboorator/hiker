d615d129554b87ab922735a11b58f0654d45b296
naboorator <zbarek@gmail.com>
2026-09-18 08:07:45 +0200

    deply fix

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -3,8 +3,7 @@ import type { MapboxConfig } from '../app/core/interface/mapbox-config.interface
 export const environment = {
   apiOrigin: 'http://localhost:3000',
   mapbox: {
-    accessToken:
-      '',
+    accessToken: '',
     styleUrl: 'mapbox://styles/mapbox/outdoors-v12',
     routeColor: '#ed7a32',
   } satisfies MapboxConfig,
